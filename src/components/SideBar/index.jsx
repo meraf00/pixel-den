207505e207e52d6598b417ec420c068d1bee35c6
Leul Wujira <meraf000@gmail.com>
2023-07-21 02:58:59 +0300

    feat(authentication): add sign out button

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,6 +6,8 @@ import {
   FolderOutlined,
 } from "@ant-design/icons";
 import { Link, useLocation } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import { YesNoDialog } from "components/Dialog";
 
 export const Icon = (props) => {
   const { children, text, active, ...rest } = props;
@@ -39,6 +41,7 @@ export const Icon = (props) => {
 
 export default function SideBar(props) {
   const [isOpen, setOpen] = useState(true);
+  const [showLogout, setShowLogout] = useState(false);
 
   const toggleBar = () => {
     setOpen((isOpen) => !isOpen);
@@ -48,8 +51,9 @@ export default function SideBar(props) {
   let settingClassName = "";
 
   if (!isOpen) {
-    sidebarClassName = "translate-x-full";
-    settingClassName = "-translate-x-16 -rotate-90";
+    sidebarClassName = "translate-x-full md:translate-x-0";
+    settingClassName =
+      "-translate-x-16 -rotate-90 md:rotate-0 md:translate-x-0";
   }
 
   const paths = ["/settings/account", "/settings/payment", "/settings/assets"];
@@ -61,53 +65,76 @@ export default function SideBar(props) {
   );
 
   return (
-    <div
-      className={`fixed top-0 -right-3 md:left-0 h-full pt-16 md:w-4/12 max-w-xs md:pt-7 md:z-50 ${sidebarClassName}
+    <>
+      <div
+        className={`
+      fixed top-0 -right-3 md:left-0 h-full 
+      pt-16 md:w-4/12 max-w-xs md:pt-7 z-10 md:z-50 ${sidebarClassName}
       md:text-black md:bg-white
       flex flex-col justify-between 
       transition-all duration-500 delay-75
     `}
-    >
-      <div>
-        {/* settings title / toggler */}
-        <div
-          className={`flex items-center mb-5 gap-4 p-3 md:pl-3 md:border-b md:mx-5`}
-        >
+      >
+        <div>
+          {/* settings title / toggler */}
           <div
-            onClick={toggleBar}
-            className={`w-fit text-2xl flex items-center saturate-200 md:bg-white rounded-full md:p-2 md:pointer-events-none transition-all duration-500 ${settingClassName}`}
+            className={`flex items-center mb-5 gap-4 p-3 md:pl-3 md:border-b md:mx-5`}
           >
-            <SettingOutlined />
+            <div
+              onClick={toggleBar}
+              className={`w-fit text-2xl flex items-center saturate-200 md:bg-white rounded-full md:p-2 md:pointer-events-none transition-all duration-500 ${settingClassName}`}
+            >
+              <SettingOutlined />
+            </div>
+            <div className="w-0 md:w-full overflow-hidden whitespace-nowrap font-semibold">
+              Settings
+            </div>
           </div>
-          <div className="w-0 md:w-full overflow-hidden whitespace-nowrap font-semibold">
-            Settings
+
+          {/* tabs */}
+          <div className="md:pl-8">
+            <Link to={paths[0]}>
+              <Icon text="Account" active={activeIndex === 0}>
+                <UserOutlined />
+              </Icon>
+            </Link>
+
+            <Link to={paths[1]}>
+              <Icon text="Payment" active={activeIndex === 1}>
+                <MoneyCollectOutlined />
+              </Icon>
+            </Link>
+
+            <Link to={paths[2]}>
+              <Icon text="My assets" active={activeIndex === 2}>
+                <FolderOutlined />
+              </Icon>
+            </Link>
           </div>
         </div>
 
-        {/* tabs */}
-        <div className="md:pl-8">
-          <Link to={paths[0]}>
-            <Icon text="Account" active={activeIndex === 0}>
-              <UserOutlined />
-            </Icon>
-          </Link>
-
-          <Link to={paths[1]}>
-            <Icon text="Payment" active={activeIndex === 1}>
-              <MoneyCollectOutlined />
-            </Icon>
-          </Link>
-
-          <Link to={paths[2]}>
-            <Icon text="My assets" active={activeIndex === 2}>
-              <FolderOutlined />
-            </Icon>
-          </Link>
-        </div>
+        {/* botttom button  */}
+        <button
+          className="flex justify-center items-center gap-3 p-3 m-2 rounded bg-secondary-200 font-semibold hover:text-white md:hover:bg-secondary-300 saturate-200"
+          onClick={() => setShowLogout(!showLogout)}
+        >
+          <span className="flex items-center ">
+            <FiLogOut className="" />
+          </span>
+          <span className="hidden md:flex items-center">Sign out</span>
+        </button>
       </div>
 
-      {/* botttom button  */}
-      <div className="hidden md:block pl-3 md:pl-8">Sign out</div>
-    </div>
+      {showLogout && (
+        <YesNoDialog
+          message="Would you like to log out?"
+          onYes={() => {
+            setShowLogout(false);
+            props.handleLogout();
+          }}
+          onNo={() => setShowLogout(false)}
+        />
+      )}
+    </>
   );
 }
