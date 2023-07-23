7794203060143616e8aff13716fc16665a3a48b9
Leul Wujira <meraf000@gmail.com>
2023-07-23 22:13:51 +0300

    fix: change separator component to accept children

--- src/components/Separator/index.jsx
+++ src/components/Separator/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
-export default function Separator(props) {
+export default function Separator({ children, animateOnHover, className }) {
+  if (animateOnHover) {
+    return (
+      <div className="flex gap-2 items-center group">
+        <hr
+          className={`flex-grow opacity-30 group-hover:border-2
+          group-hover:border-secondary-100 saturate-200          
+          group-hover:opacity-100 transition-color duration-300 ${className}`}
+        />
+        <span
+          className="opacity-80         
+        group-hover:opacity-100
+        group-hover:text-black
+        transition-color duration-300
+        saturate-200"
+        >
+          {children}
+        </span>
+        <hr
+          className={`flex-grow opacity-30 group-hover:border-2
+          group-hover:border-secondary-100 saturate-200 
+          group-hover:opacity-100 transition-color duration-300 ${className}`}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="flex gap-2 items-center">
       <hr className="flex-grow opacity-30" />
-      <span className="opacity-80">{props.text}</span>
+      <span className="opacity-80">{children}</span>
       <hr className="flex-grow opacity-30" />
     </div>
   );
