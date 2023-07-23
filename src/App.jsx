d280f579661a112517c04e69c2d3534bf5da24d3
Leul Wujira <meraf000@gmail.com>
2023-07-23 22:16:58 +0300

    fix: move footer to app root

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "layout/Footer";
 import Navbar from "./layout/Navbar";
 import { Outlet } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
